cc5f2afb127d89924c5d935ca70e820229ae9e6b
lsp <spieled916@gmail.com>
2014-09-27 17:22:33 +0800

    1、在CommonUtil模块中添加操作时间的工具方法 2、新建mygit包，计划在其中实现获取git日志功能的工具方法

--- myemail/CommonUtil.py
+++ myemail/CommonUtil.py
@@ -68,23 +68,117 @@ def get_file_md5(path=None):
     file.close()
     return file_md5
 
+
+def get_current_timestamp():
+    """
+    获取当前时间戳
+    """
+    from datetime import datetime
+    now = datetime.now()
+    return now.timestamp()
+
+
+def get_current_nanosecond():
+    """
+    获取当前时间纳秒
+    """
+    from datetime import datetime
+    return int(datetime.now().timestamp() * 1000000)
+
+
+def get_current_microsecond():
+    """
+    获取当前时间毫秒
+    """
+    from datetime import datetime
+    return int(datetime.now().timestamp() * 1000)
+
+
+def get_current_second():
+    """
+    获取当前秒
+    """
+    from datetime import datetime
+    return int(datetime.now().timestamp())
+
+
+def get_current_ymd(timestamp=None):
+    """
+    获取当前年月日
+    timestamp单位 秒
+    """
+    from datetime import datetime
+    now = datetime.now()
+    if isinstance(timestamp, int) or isinstance(timestamp, float):
+        now = datetime.fromtimestamp(timestamp)
+    return now.year * 10000 + now.month * 100 + now.day
+
+
+def get_current_ym(timestamp=None):
+    """
+    获取当前年月
+    timestamp单位 秒
+    """
+    from datetime import datetime
+    now = datetime.now()
+    if isinstance(timestamp, int) or isinstance(timestamp, float):
+        now = datetime.fromtimestamp(timestamp)
+    return now.year * 100 + now.month
+
+
+def get_current_y(timestamp=None):
+    """
+    获取当前年月
+    timestamp单位 秒
+    """
+    from datetime import datetime
+    now = datetime.now()
+    if isinstance(timestamp, int) or isinstance(timestamp, float):
+        now = datetime.fromtimestamp(timestamp)
+    return now.year
+
+
+def get_human_time(timestamp=None):
+    """
+    获取自然人可读的时间表示
+    """
+    from datetime import datetime
+    now = datetime.now()
+    if isinstance(timestamp, int) or isinstance(timestamp, float):
+        now = datetime.fromtimestamp(timestamp)
+    return str(now.year).zfill(4) + '-' + str(now.month).zfill(2) + '-' + str(now.day).zfill(2) + ' ' + \
+           str(now.hour).zfill(2) + ':' + str(now.minute).zfill(2) + ':' + str(now.second).zfill(2)
+
 if __name__ == '__main__':
-    print('extension of /tmp/log.log.txt is', get_extension('/tmp/log.log.txt'))
-    print('extension of /tmp/log.log is', get_extension('/tmp/log.log'))
-    print('extension of /tmp/log is', get_extension('/tmp/log'))
+    # print('extension of /tmp/log.log.txt is', get_extension('/tmp/log.log.txt'))
+    # print('extension of /tmp/log.log is', get_extension('/tmp/log.log'))
+    # print('extension of /tmp/log is', get_extension('/tmp/log'))
+    #
+    # print(has_text())
+    # print(has_text(""))
+    # print(has_text("      "))
+    # print(has_text("    \t \n\r "))
+    # print(has_text("   p  "))
+    #
+    # print(md5('123456'))
+    # print(md5())
+    # print(md5(''))
+    # print(md5('  '))
+    #
+    # print(get_file_md5())
+    # print(get_file_md5('./CommonUtil.py'))
+    # print(get_file_md5('README'))
+    # print(get_file_md5('bat.jpg'))
 
-    print(has_text())
-    print(has_text(""))
-    print(has_text("      "))
-    print(has_text("    \t \n\r "))
-    print(has_text("   p  "))
+    print(get_current_timestamp())
+    print(get_current_nanosecond())
+    print(get_current_microsecond())
+    print(get_current_second())
+    print(get_current_ymd())
+    print(get_current_ym())
+    print(get_current_y())
 
-    print(md5('123456'))
-    print(md5())
-    print(md5(''))
-    print(md5('  '))
+    print(get_current_ymd(1311808262.762649))
+    print(get_current_ymd(131180))
 
-    print(get_file_md5())
-    print(get_file_md5('./CommonUtil.py'))
-    print(get_file_md5('README'))
-    print(get_file_md5('bat.jpg'))
+    print(get_human_time())
