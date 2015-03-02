c3b9b928dcf93532e8912b74d8cf3c4bafc41aeb
刘少平 <472458220@qq.com>
2015-03-02 14:26:59 +0800

    Update clear-log.py
    微调打印信息

--- clear-log.py
+++ clear-log.py
@@ -1,26 +1,21 @@
 import os
 import os.path
 toppath = '/app'
-print('is going to list /app')
 for f in os.listdir(toppath):
-    print(f)
     fpath = os.path.join(toppath, os.path.basename(f))
-    print(fpath)
     if os.path.isdir(fpath):
         logdir = os.path.join(fpath, 'tomcat', 'logs')
-        print(logdir)
         if os.path.exists(logdir):
-            print(' actually exists')
             for _f in os.listdir(logdir):
                 basename = os.path.basename(_f)
-                print('basename is ', basename)
+                fullpath = os.path.join(logdir, basename)
                 if basename == 'catalina.out':
-                    print('actually got you -- catalina.out, execute echo "" > ' + os.path.abspath(_f))
-                    os.system('echo "" > ' + os.path.join(logdir, basename)) # echo '' > catalina.out
+                    print('execute echo "" > ' + fullpath)
+                    os.system('echo "" > ' + fullpath) # echo '' > catalina.out
                 else:
-                    print('haha , you will be deleted')
-                    os.remove(os.path.join(logdir, basename))
+                    print('execute rm ' + fullpath)
+                    os.remove(fullpath)
         else:
-            print(' not exists')
+            print(logdir + ' is not exists')
     else:
-        print(fpath, ' is not a dir')
+        print(fpath + ' is not a dir')
